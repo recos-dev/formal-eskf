8ff069d9fa843d830f03f016cb3e4d31814ebdb2
Shengwen Cheng <shengwen1997.tw@gmail.com>
2026-09-11 19:51:45 +0800

    Add license headers to Lean sources

--- formal/lean/FormalESKF/RotationVector.lean
+++ formal/lean/FormalESKF/RotationVector.lean
@@ -1,3 +1,11 @@
+/-
+formal-eskf is freely redistributable under the BSD 3-Clause License.
+See the file "LICENSE" for information on usage and redistribution of this
+file.
+
+SPDX-License-Identifier: BSD-3-Clause
+-/
+
 import FormalESKF.SO3
 import Mathlib.Analysis.Calculus.Taylor
 import Mathlib.Analysis.SpecialFunctions.Trigonometric.ArctanDeriv
